e9a7e0b3b3fae0f9ce44ee4fe8f2356260f940b7
wwdd1 <mhmtctn35@gmail.com>
2024-06-19 13:12:30 +0300

    test-1

--- app/api/static/attorneys/route.ts
+++ app/api/static/attorneys/route.ts
@@ -1,4 +1,6 @@
-export function GET (req) {
+import { NextRequest } from "next/server"
+
+export function GET (req: NextRequest) {
   console.log('code is: ', req.nextUrl.searchParams.get('code'))
   return new Response('Hello world')
 }
